f15911644fd9130ae9a7682e260e2c59373fd6ba
tomek <tomekzbroszczyk@gmail.com>
2023-10-30 22:02:47 +0100

    Added Selection Sort and fixed Merge Sort

--- lang/en/projects/projects/sorts/sortsRaw.js
+++ lang/en/projects/projects/sorts/sortsRaw.js
@@ -116,5 +116,26 @@ async function partition(arr, low, high) {
     return i + 1;
 }
 
+function selectionSort(arr) {
+    const n = arr.length;
 
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            // Swap arr[i] and arr[minIndex]
+            const temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    return arr;
+}
 
