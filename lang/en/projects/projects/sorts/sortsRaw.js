b898211df15e93d9376b6c2c19362f813ff5a379
tomek <tomekzbroszczyk@gmail.com>
2023-10-30 22:33:52 +0100

    Added DNF Sort

--- lang/en/projects/projects/sorts/sortsRaw.js
+++ lang/en/projects/projects/sorts/sortsRaw.js
@@ -180,9 +180,29 @@ function gnomeSort(arr) {
         if (arr[index] >= arr[index - 1]) {
             index++;
         } else {
-            // Swap the elements
             [arr[index], arr[index - 1]] = [arr[index - 1], arr[index]];
             index--;
+        }
+    }
+
+    return arr;
+}
+
+function dutchNationalFlagSort(arr) {
+    let low = 0;
+    let mid = 0;
+    let high = arr.length - 1;
+
+    while (mid <= high) {
+        if (arr[mid] === 0) {
+            [arr[low], arr[mid]] = [arr[mid], arr[low]];
+            low++;
+            mid++;
+        } else if (arr[mid] === 1) {
+            mid++;
+        } else {
+            [arr[mid], arr[high]] = [arr[high], arr[mid]];
+            high--;
         }
     }
 
