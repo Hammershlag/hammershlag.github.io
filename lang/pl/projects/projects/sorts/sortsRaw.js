7fa417888f8a467cfa2eca660759c815eaa0b4fb
tomek <tomekzbroszczyk@gmail.com>
2024-03-07 20:25:24 +0100

    Polish site, sorts now match visuals and home redirect fix

---
+++ lang/pl/projects/projects/sorts/sortsRaw.js
@@ -0,0 +1,210 @@
+function sortsRaw(arr) {
+    let len = arr.length;
+    let swapped;
+
+    do {
+        swapped = false;
+
+        for (let i = 0; i < len - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    return arr;
+}
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currentElement = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > currentElement) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = currentElement;
+    }
+
+    return arr;
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+function isSorted(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < arr[i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+
+function bogoSort(arr) {
+    while (!isSorted(arr)) {
+        shuffleArray(arr);
+    }
+    return arr;
+}
+
+async function quickSort(arr, low, high) {
+    if (low < high) {
+        const pivotIndex = await partition(arr, low, high);
+        await quickSort(arr, low, pivotIndex - 1);
+        await quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+async function partition(arr, low, high) {
+    const pivot = arr[high];
+    let i = low - 1;
+
+    for (let j = low; j < high; j++) {
+
+        if (arr[j] < pivot) {
+            i++;
+            const temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    const temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+function selectionSort(arr) {
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            const temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    return arr;
+}
+
+function cocktailShakerSort(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    let swapped = true;
+
+    while (left < right && swapped) {
+        swapped = false;
+
+        for (let i = left; i < right; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swapped = true;
+            }
+        }
+
+        right--;
+
+        for (let i = right; i > left; i--) {
+            if (arr[i] < arr[i - 1]) {
+                [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
+                swapped = true;
+            }
+        }
+
+        left++;
+    }
+
+    return arr;
+}
+
+function gnomeSort(arr) {
+    const n = arr.length;
+    let index = 0;
+
+    while (index < n) {
+        if (index === 0) {
+            index++;
+        }
+
+        if (arr[index] >= arr[index - 1]) {
+            index++;
+        } else {
+            [arr[index], arr[index - 1]] = [arr[index - 1], arr[index]];
+            index--;
+        }
+    }
+
+    return arr;
+}
+
+function dutchNationalFlagSort(arr) {
+    let low = 0;
+    let mid = 0;
+    let high = arr.length - 1;
+
+    while (mid <= high) {
+        if (arr[mid] === 0) {
+            [arr[low], arr[mid]] = [arr[mid], arr[low]];
+            low++;
+            mid++;
+        } else if (arr[mid] === 1) {
+            mid++;
+        } else {
+            [arr[mid], arr[high]] = [arr[high], arr[mid]];
+            high--;
+        }
+    }
+
+    return arr;
+}
